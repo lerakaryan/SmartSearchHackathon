cf7f4846aaf38fbf8b3ef7f7eb49a973c5111fb3
veehsub <vladimirersh334@gmail.com>
2025-09-20 12:57:00 +0300

    фронт

--- SmartSearchFront/src/app/services/search-service/search-service.service.ts
+++ SmartSearchFront/src/app/services/search-service/search-service.service.ts
@@ -3,17 +3,24 @@ import { inject, Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { RawData } from '../../interfaces/rawData';
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class SearchServiceService {
-private apiUrl = environment.apiUrl +'/search';
+private apiUrl = environment.apiUrl +'/pages';
 http = inject(HttpClient);
   constructor() { }
 
   sendData(rawData: RawData) 
   {
     console.log(rawData);
-    return this.http.post(this.apiUrl, rawData);
+    return this.http.post(this.apiUrl, rawData); //тут ответ список статей
   }
+
+  getPageById(id: number)
+{
+return this.http.get(this.apiUrl+'/'+id);
+}
+
 }
